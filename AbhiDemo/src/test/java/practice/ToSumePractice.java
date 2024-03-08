8397064b2587ee434aa8153f5ad084d5ccbee290
kiran <kiran@Kiran>
2024-03-08 21:44:45 +0530

    nkn class add maddhe

---
+++ AbhiDemo/src/test/java/practice/ToSumePractice.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToSumePractice {
+
+	@Test
+	public void kalthe() {
+		Reporter.log("Its fuckin done",true);
+	}
+}
